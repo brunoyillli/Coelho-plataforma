36e19ab7de0eb1fda4053d2fe5802d38e196bc33
brunoyillli <bruboneet@gmail.com>
2020-03-20 23:14:53 -0300

    pulo

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,27 @@
 extends KinematicBody2D
 
+const velX = 500
 const grav = 1500
-var velocity = Vector2(500, 0)
+var velocity = Vector2(0, 0)
+var jump  = false
 func _ready():
-	pass
+	set_process_input(true)
+	
 
 func _physics_process(delta):
 	velocity.y += grav * delta
+	velocity.x = velX
 	velocity = move_and_slide(velocity,Vector2(0,-1))
-	pass
+	
+	if is_on_floor():
+		$sprite.play("walk")
+		if jump:
+			velocity.y = -800
+	else:
+		$sprite.play("jump")		
+	jump = false
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if  event.is_pressed():
+			jump = true
