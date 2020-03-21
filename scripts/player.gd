88142069b4420a5f400dbdafe2234663171241bb
brunoyillli <bruboneet@gmail.com>
2020-03-20 22:35:33 -0300

    adicionando velocidade e modelo de chao

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 
-
+const grav = 1500
+var velocity = Vector2(500, 0)
 func _ready():
 	pass
 
 func _physics_process(delta):
+	velocity.y += grav * delta
+	velocity = move_and_slide(velocity,Vector2(0,-1))
 	pass
