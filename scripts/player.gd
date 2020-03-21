add0d0cf2d23320334a80c8242a120362749fd06
brunoyillli <bruboneet@gmail.com>
2020-03-21 13:10:40 -0300

    boing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
-const velX = 500
+const velX = 800
 const grav = 1500
 var velocity = Vector2(0, 0)
 var jump  = false
 var jump_release = false
+var was_on_floor = false
 func _ready():
 	set_process_input(true)
 	
@@ -15,6 +16,8 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity,Vector2(0,-1))
 	
 	if is_on_floor():
+		if not was_on_floor:
+			$anim_lander.play("boing")
 		$sprite.play("walk")
 		if jump:
 			velocity.y = -1000
@@ -23,7 +26,7 @@ func _physics_process(delta):
 		if jump_release:
 			velocity.y *= .3
 			
-					
+	was_on_floor = is_on_floor()				
 	jump = false
 	jump_release = false
 	
