434de6bdfb394ab8e489104fb76b2cf28a7ec06b
brunoyillli <bruboneet@gmail.com>
2020-05-22 20:29:41 -0300

    criando menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,24 @@
 extends Node2D
 
+enum {MENU , LOADING , LOADED}
+
+var status = MENU
+var current_stage
+var loaded_stage
 
 func _ready():
 	add_to_group("game")
 	
 func stage_selected(button):
-	print(button.stage)
+	if status == MENU:
+		status = LOADING
+		current_stage = button.stage
+		$interfase.hide()
+		load_stage()
+		status = LOADED
+	
+func load_stage():
+	if loaded_stage != null:
+		loaded_stage.queue_free()
+	loaded_stage = load(current_stage).instance()
+	add_child(loaded_stage)
