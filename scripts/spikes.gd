bbf836e21016c7f86e3fe81af34f06762837222b
brunoyillli <bruboneet@gmail.com>
2020-05-22 19:43:42 -0300

    Armadilhas

---
+++ scripts/spikes.gd
@@ -0,0 +1,23 @@
+tool
+extends Area2D
+
+var textures = [
+	"res://sprites/Environment/spikes_top.png",
+	"res://sprites/Environment/spikes_bottom.png"
+]
+export(int , "Top" , "Botton")var texture = 0 setget set_texture
+
+func _ready():
+	pass
+	
+func _draw():
+	$sprite.texture = load(textures[texture])
+
+func set_texture(val):
+	texture = val
+	if is_inside_tree() and Engine.editor_hint:
+		update();
+
+
+func _on_spikes_body_entered(body):
+	body.killed()
